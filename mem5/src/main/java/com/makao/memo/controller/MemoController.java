3b665b61c899a90e492fec8b5696e78ca84abcc7
yunsol <c21154035@gmail.com>
2020-03-14 11:36:13 +0900

    메모 노트타입 작성 진행중

--- mem5/src/main/java/com/makao/memo/controller/MemoController.java
+++ mem5/src/main/java/com/makao/memo/controller/MemoController.java
@@ -10,7 +10,7 @@ import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,51 +24,71 @@ import com.makao.memo.util.AuthInfo;
 public class MemoController
 {
 	private static final Logger logger = Logger.getLogger(MemoController.class);
-	
-	public MemoController() {
-		if (logger.isInfoEnabled()) {
+
+	public MemoController()
+	{
+		if (logger.isInfoEnabled())
+		{
 			logger.info("MemoController()");
 		}
 	}
 
 	@Autowired
 	private MemoService service;
-	
-	@RequestMapping(value = "/memo/allMyList", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(value = "/memo/allMyList", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public Map<String,Object> getAllMyMemo(HttpSession session) throws Exception {
+	public Map<String, Object> getAllMyMemo(HttpSession session) throws Exception
+	{
 		AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
 		List<Memo> list = service.getAllMemo(authInfo.getId());
-		Map<String,Object> resMap = new HashMap<String, Object>();
+		Map<String, Object> resMap = new HashMap<String, Object>();
 		resMap.put("array", list);
-		System.out.println(list.size());
 		return resMap;
 	}
-	
+
 	@RequestMapping(value = "/memo/selectType", method = RequestMethod.GET)
-	public ModelAndView goAdd(Long ctgrId) throws Exception {
+	public ModelAndView goAdd(Long ctgrId) throws Exception
+	{
 		ModelAndView mv = new ModelAndView("memo/selectType");
-		if (null != ctgrId) {
+		if (null != ctgrId)
+		{
 			mv.addObject("ctgrId", ctgrId);
 		}
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "/memo/add", method = RequestMethod.GET)
-	public ModelAndView goAdd(Long ctgrId, int type) throws Exception {
+	public ModelAndView goAdd(Long ctgrId, int type) throws Exception
+	{
 		ModelAndView mv = new ModelAndView();
-		if (null != ctgrId) {
+		if (null != ctgrId)
+		{
 			mv.addObject("ctgrId", ctgrId);
 		}
-		
-		if (type == Memo.TYPE_NOTE) {
+
+		if (type == Memo.TYPE_NOTE)
+		{
 			mv.setViewName("memo/addNote");
-		} else if(type==Memo.TYPE_TODO) {
+		}
+		else if (type == Memo.TYPE_TODO)
+		{
 			mv.setViewName("memo/addTodo");
 		}
 		return mv;
 	}
-	
-	
+
+	@RequestMapping(value = "/memo/saveNote", method = RequestMethod.POST)
+	public ModelAndView saveNote(@ModelAttribute Memo memo, HttpSession session) throws Exception
+	{
+		ModelAndView mv = new ModelAndView();
+		AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+		if (null != authInfo) {
+			memo.setRegUserId(authInfo.getId());
+		}
+
+		service.addMemo(memo);
+		return mv;
+	}
 
 }
--- mem5/src/main/webapp/WEB-INF/views/memo/addNote.jsp
+++ mem5/src/main/webapp/WEB-INF/views/memo/addNote.jsp
@@ -5,5 +5,50 @@
   <%@ include file="/WEB-INF/views/includes/00_head.jsp" %>
 </head>
 <body>
+<div class="col-md-12">
+<c:choose>
+	<c:when test="${empty authInfo}">
+		 <h4 class="mb-3">로그인을 먼저 해주세요</h4>
+	</c:when>
+	<c:otherwise>
+	      <h4 class="mb-3">Write Note</h4>
+	      <form class="needs-validation" novalidate="" method="post" action="${pageContext.request.contextPath }/memo/saveNote">
+	          <div class="mb-3">
+	            <label for="title">Title</label>
+	            <input type="text" class="form-control" id="title" placeholder="" value="" required="">
+	            <div class="invalid-feedback">
+	              Valid title is required.
+	            </div>
+	          </div>
+	
+	        <div class="mb-3">
+	          <label for="username">Username</label>
+	          <div class="input-group">
+	            <div class="input-group-prepend">
+	              <span class="input-group-text">@</span>
+	            </div>
+	            <input type="hidden" id="regUserId" value="${authInfo.id}">
+	            <input type="text" class="form-control" placeholder="Username" required="" value="${authInfo.name}" readonly>
+	          </div>
+	        </div>
+	
+	        <div class="mb-3">
+	          <label for="tag">TAG <span class="text-muted">(Optional)</span></label>
+	          <input type="text" class="form-control" id="tag" placeholder="쉼표(,)를 구분해서 입력해주세요">
+	          <div class="invalid-feedback">
+	            쉼표(,)를 구분해서 입력해주세요
+	          </div>
+	        </div>
+	
+	        <div class="mb-3">
+	          <label for="content">Content</label>
+	          <textarea type="text" class="form-control" id="content" required=""></textarea>
+	        </div>
+	        <hr class="mb-4">
+	        <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+	      </form>
+	</c:otherwise>
+</c:choose>
+</div>
 </body>
 </html>
